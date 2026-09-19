1bd42d3355bd84b4914cd23d00992e65b40576bd
liqiang <betta@bettas-MacBook-Pro-4.local>
2026-09-20 00:01:30 +0800

    添加协程单例

---
+++ Core/Utils/EnumeratorController.cs
@@ -0,0 +1,14 @@
+﻿
+namespace GameSDK
+{
+    /// <summary>
+    /// 协程管理器，通过继承的 MonoBehaviour 方法启动和停止协程。
+    /// </summary>
+    public class EnumeratorController : SingletonMono<EnumeratorController>
+    {
+        private EnumeratorController()
+        {
+        }
+    }
+
+}
