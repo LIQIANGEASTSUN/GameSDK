18e0b8bc1333248a4418b550c1d9c7454d9465a0
liqiang <betta@bettas-MacBook-Pro-4.local>
2026-09-19 18:30:23 +0800

    DontDestroy

---
+++ Core/Utils/DontDestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GameSDK
+{
+    /// <summary>
+    /// 切场景不自动销毁的 GameObject 
+    /// </summary>
+    public class GameObjectDontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
